a6165ec0bd083cb84a1f7cef63ae4888d77774e1
Aiman Haziq <153014908+AlloraCavendish@users.noreply.github.com>
2025-01-13 20:34:07 +0800

    Update API Key

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import { dirname, join } from 'path';
 const app = express();
 const port = process.env.PORT;
 
-const API_KEY = process.env.API_KEY;
+const API_KEY = AIzaSyDRQq2myYcUc-u6j8AQh_9X4-yQTDL5KLY;
 const API_URL = `https://ipqualityscore.com/api/json/url/${API_KEY}`;
 
 const corsOptions = {
